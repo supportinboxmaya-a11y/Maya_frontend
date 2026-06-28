0661454dcb3318aec6e6d0da772deeaf5c9072ae
supportinboxmaya-a11y <supportinbox.maya@gmail.com>
2026-06-28 16:37:03 +0530

    add src/components/layout/Layout.tsx

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -7,9 +7,9 @@ export function Layout() {
   return (
     <div className='flex h-screen bg-[#0a0b0f] overflow-hidden'>
       <Sidebar/>
-      <div className='flex-1 flex flex-col ml-60 min-w-0'>
+      <div className='flex-1 flex flex-col md:ml-60 min-w-0'>
         <TopBar/>
-        <main className='flex-1 overflow-y-auto p-6'>
+        <main className='flex-1 overflow-y-auto p-4 md:p-6'>
           <Outlet/>
         </main>
       </div>
