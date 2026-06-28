200a339c183ebdc57cc7bb1ec5f89f08e8699357
supportinboxmaya-a11y <supportinbox.maya@gmail.com>
2026-06-28 13:28:47 +0530

    add src/components/layout/Layout.tsx

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,12 +1,19 @@
 import { Outlet } from 'react-router-dom'
 import { Sidebar } from './Sidebar'
 import { TopBar } from './TopBar'
+import { CommandPalette } from '@/components/CommandPalette'
+
 export function Layout() {
-  return <div className="flex h-screen overflow-hidden bg-[#0a0b0f]">
-    <Sidebar/>
-    <div className="flex-1 flex flex-col overflow-hidden ml-60">
-      <TopBar/>
-      <main className="flex-1 overflow-y-auto p-6"><Outlet/></main>
+  return (
+    <div className='flex h-screen bg-[#0a0b0f] overflow-hidden'>
+      <Sidebar/>
+      <div className='flex-1 flex flex-col ml-60 min-w-0'>
+        <TopBar/>
+        <main className='flex-1 overflow-y-auto p-6'>
+          <Outlet/>
+        </main>
+      </div>
+      <CommandPalette/>
     </div>
-  </div>
+  )
 }
